86307bc22bf013258757d8d387e1a4c5dc92383b
pabloTokuo <pbo_1996@hotmail.com>
2023-09-07 16:12:46 -0300

    Parameters JS

---
+++ JavaScript/parameters/parameters.js
@@ -0,0 +1,10 @@
+const printGreetings = (message = 'Mensagem Padrão') => {
+  /* 
+    Caso não passe nenhum valor
+    Quando chamar a funcao
+    Vai receber o valor do parametro
+  */
+  console.log(message);
+}
+
+printGreetings('Seja bem vindo(a)');
