b53449436db43a49979b0e496b7a6d0d47923ed0
pabloTokuo <pbo_1996@hotmail.com>
2023-09-24 22:31:33 -0300

    Modules JS

---
+++ JavaScript/modules/index.js
@@ -0,0 +1,5 @@
+import userName ,{sayHello as novoName} from "./sayHello.js";
+import { sayHello } from "./sayHello.js";
+
+sayHello(userName);
+novoName('Pablo');
---
+++ JavaScript/modules/sayHello.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+  console.log(`Olá, ${name}`);
+}
+
+export {sayHello};
+// export default sayHello;
+
+const userName = 'Pablo';
+
+export default userName;
