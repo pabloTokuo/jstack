7f9a2b1f19ebf98f23c5842fb18341792eecabfa
pabloTokuo <pbo_1996@hotmail.com>
2023-09-28 22:27:56 -0300

    Helpers JS

---
+++ JavaScript/helpers/helpers.js
@@ -0,0 +1,15 @@
+// Includes
+const tech = 'Node.js';
+const array = ['Node.js', 'React', 'TypeScript'];
+
+const includesString = tech.includes('a');
+const includesArray = array.includes('React');
+console.log({includesArray});
+
+// Startswith
+const startWith = tech.startsWith('no');
+console.log({startWith});
+
+// Endswith
+const endsWith = tech.endsWith('s');
+console.log({endsWith});
