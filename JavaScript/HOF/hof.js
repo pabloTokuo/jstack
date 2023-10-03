49d1f228dc8ac85b20b0e0ba5a04f468b67e07f7
pabloTokuo <pbo_1996@hotmail.com>
2023-10-02 22:04:13 -0300

    map reduce reduce JS

--- JavaScript/HOF/hof.js
+++ JavaScript/HOF/hof.js
@@ -22,4 +22,28 @@ const some = array.some((product) => product.price < 1000);
 // every
 // Verifica se todos os items do array bate com a condicao
 const every = array.every((product) => product.price > 900);
-console.log({every});
+// console.log({every});
+
+// map
+// Retorna um novo array com as mudancas necessarias
+const map = array.map((product) => {
+  return {
+    ...product,
+    subtotal: product.price * product.quantity,
+  }
+});
+// console.log({map});
+
+// filter
+// Filtra e retorna um array com a condicao especifica
+const filter = array.filter((product) => product.quantity > 1);
+// console.log(filter);
+
+// reduce
+// Reduce recebe 2 argumentos
+// 
+const reduce = array.reduce((acc, product) => {
+  return acc + (product.price * product.quantity);
+}, 0);
+
+console.log({ reduce });
