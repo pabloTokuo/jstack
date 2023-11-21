b7cb4fc76293962554b6983d6afa8f1a322e7837
pabloTokuo <pbo_1996@hotmail.com>
2023-11-21 19:16:36 -0300

    Props e Childrens react

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
 
+import Post from "./Post";
+import Header from "./Header";
+
 function App() {
-  return <h1>Componente App</h1>
+  return (
+    <>
+      <Header title="JStack's Blog">
+        <h2>Posts da semana</h2>
+      </Header>
+
+      <hr />
+
+      <Post 
+        title="Título da notícia 01" 
+        subtitle="Subtítulo da notícia 01"
+      />
+      <Post 
+        title="Título da notícia 02" 
+        subtitle="Subtítulo da notícia 02"
+      />
+      <Post 
+        title="Título da notícia 03" 
+        subtitle="Subtítulo da notícia 03"
+      />
+    </>
+  )
 }
 
 export default App;
---
+++ react-fundamentos/src/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Post(props) {
+  return (
+    <>
+      <article>
+        <strong>{props.title}</strong>
+        <br />
+        <small>{props.subtitle}</small>
+      </article>
+      <br />
+    </>
+  );
+}
