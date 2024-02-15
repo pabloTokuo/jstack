b39918b6f9fb5994f0d65798c2bc11f9339aeda6
pabloTokuo <pbo_1996@hotmail.com>
2024-02-15 08:20:06 -0300

    using useEffect

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,30 +1,32 @@
-import React, { useState, useMemo } from 'react';
-import { ThemeProvider } from 'styled-components';
+import React, { useState, useMemo, useEffect } from "react";
+import { ThemeProvider } from "styled-components";
 
-import GlobalStyle from './styles/global';
-import Layout from './components/Layout';
+import GlobalStyle from "./styles/global";
+import Layout from "./components/Layout";
 
-import themes from './styles/themes';
+import themes from "./styles/themes";
 
 function App() {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
 
   const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark ;
+    return themes[theme] || themes.dark;
   }, [theme]);
 
   const handleToggleTheme = () => {
-    setTheme(prevState => prevState === "dark" ? "light" : "dark");
-  }
+    setTheme((prevState) => (prevState === "dark" ? "light" : "dark"));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   return (
     <ThemeProvider theme={currentTheme}>
       <GlobalStyle />
-      <Layout 
-        onToggleTheme = {handleToggleTheme} 
-        selectedTheme={theme} />
+      <Layout onToggleTheme={handleToggleTheme} selectedTheme={theme} />
     </ThemeProvider>
   );
-};
+}
 
 export default App;
